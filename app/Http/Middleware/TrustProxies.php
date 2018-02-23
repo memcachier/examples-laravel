257c83f1031d9a785f8fb2554d5b87b7cf65ef47
Sascha <sascha.trifunovic@gmail.com>
2018-02-23 15:56:37 +0100

    Set trusted proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,12 +12,20 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = '**';
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var string
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+    // For Laravel 5.5
+    // protected $headers = [
+    //     Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
+    //     Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
+    //     Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
+    //     Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+    //     Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+    // ];
 }
